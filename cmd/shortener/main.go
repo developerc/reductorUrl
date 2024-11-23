a74e17a8b4f0aac1e1a0c0037e74c728b0b78437
developerc <developerc@yandex.ru>
2024-11-23 22:39:57 +0300

    next commit4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	//"fmt"
+	"io"
 	"net/http"
+	"strconv"
 )
 
-var strUrl string
+// var strUrl string
 var beginUrl string
-var cutUrl string
+
+// var cutUrl string
+var cntr int
+var mapUrl map[int]string
 
 func mainPage2(res http.ResponseWriter, req *http.Request) {
 
 	switch req.Method {
 	case http.MethodPost:
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(string(body))
+		mapUrl[cntr] = string(body)
+
+		//fmt.Println(mapUrl)
 		beginUrl = req.Host
 		//fmt.Println("beginUrl: ", beginUrl)
-		strUrl = req.URL.String()
-		strUrl = strUrl[1:]
+		//strUrl = req.URL.String()
+		//strUrl = strUrl[1:]
 		//fmt.Println("strUrl: " + strUrl)
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
 		//_, _ = res.Write([]byte(`http://localhost:8080/EwHXdJfB `))
-		res.Write([]byte("http://" + beginUrl + "/" + cutUrl))
+		res.Write([]byte("http://" + beginUrl + "/" + strconv.Itoa(cntr)))
+		cntr++
 		return
 	case http.MethodGet:
-		res.Header().Set("Location", "http://"+beginUrl+"/"+strUrl)
+		tmpStr := req.URL.String()[1:]
+		//fmt.Println(tmpStr)
+		i, err := strconv.Atoi(tmpStr)
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+		longUrl := mapUrl[i]
+		//res.Header().Set("Location", "http://"+beginUrl+"/"+strUrl)
+		res.Header().Set("Location", longUrl)
 		res.WriteHeader(http.StatusTemporaryRedirect)
+		//delete(mapUrl, i)
 		return
 		/*tmpStr := req.URL.String()[1:]
 		//fmt.Println(tmpStr + "   " + cutUrl)
@@ -86,7 +110,9 @@ func mainPage2(res http.ResponseWriter, req *http.Request) {
 }*/
 
 func main() {
-	cutUrl = "EwHXdJfB"
+	cntr = 0
+	mapUrl = make(map[int]string)
+	//cutUrl = "EwHXdJfB"
 	mux := http.NewServeMux()
 	//mux.HandleFunc(`/api/`, apiPage)
 	mux.HandleFunc(`/`, mainPage2)
