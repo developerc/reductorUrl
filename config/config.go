02488c5a310c41d2d596bfbf325a4b98612274d8
Andrey Saperov <developerc@yandex.ru>
2024-11-26 15:47:56 +0200

    next commit 20

--- config/config.go
+++ config/config.go
@@ -15,10 +15,14 @@ var ServerSettings settings
 func InitSettings() {
 	ServerSettings = settings{}
 	// указываем имя флага, значение по умолчанию и описание
-	ServerSettings.AdresRun = *flag.String("a", "localhost:8080", "address running server")
-	ServerSettings.AdresBase = *flag.String("b", "http://localhost:8080", "base address shortener URL")
+	//ServerSettings.AdresRun = *flag.String("a", "localhost:8080", "address running server")
+	//ServerSettings.AdresBase = *flag.String("b", "http://localhost:8080", "base address shortener URL")
+	ar := flag.String("a", "localhost:8080", "address running server")
+	ab := flag.String("b", "http://localhost:8080", "base address shortener URL")
 	// делаем разбор командной строки
 	flag.Parse()
+	ServerSettings.AdresRun = *ar
+	ServerSettings.AdresBase = *ab
 	fmt.Println("AdresBase: ", ServerSettings.AdresBase)
 	fmt.Println("AdresRun: ", ServerSettings.AdresRun)
 }
