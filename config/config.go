945e0534bbfb0f51d4e11a67dbcda8ef850e106c
developerc <developerc@yandex.ru>
2024-11-27 19:41:05 +0300

    next commit 23

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 type settings struct {
@@ -15,14 +16,28 @@ var ServerSettings settings
 func InitSettings() {
 	ServerSettings = settings{}
 	// указываем имя флага, значение по умолчанию и описание
-	//ServerSettings.AdresRun = *flag.String("a", "localhost:8080", "address running server")
-	//ServerSettings.AdresBase = *flag.String("b", "http://localhost:8080", "base address shortener URL")
 	ar := flag.String("a", "localhost:8080", "address running server")
 	ab := flag.String("b", "http://localhost:8080", "base address shortener URL")
 	// делаем разбор командной строки
 	flag.Parse()
-	ServerSettings.AdresRun = *ar
-	ServerSettings.AdresBase = *ab
-	fmt.Println("AdresBase: ", ServerSettings.AdresBase)
-	fmt.Println("AdresRun: ", ServerSettings.AdresRun)
+	//проверим переменную окружения SERVER_ADDRESS
+	val, ok := os.LookupEnv("SERVER_ADDRESS")
+	if !ok || val == "" {
+		ServerSettings.AdresRun = *ar
+		fmt.Println("AdresRun from flag: ", ServerSettings.AdresRun)
+	} else {
+		ServerSettings.AdresRun = val
+		fmt.Println("AdresRun from env: ", ServerSettings.AdresRun)
+	}
+
+	//проверим переменную окружения BASE_URL
+	val, ok = os.LookupEnv("BASE_URL")
+	if !ok || val == "" {
+		ServerSettings.AdresBase = *ab
+		fmt.Println("AdresBase from flag: ", ServerSettings.AdresBase)
+	} else {
+		ServerSettings.AdresBase = val
+		fmt.Println("AdresBase from env: ", ServerSettings.AdresBase)
+	}
+
 }
