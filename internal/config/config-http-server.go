28eb37299311e225449ec4c420cf92ea65e2a2f4
Andrey Saperov <developerc@yandex.ru>
2024-11-30 11:03:43 +0200

    next commit 25

--- internal/config/config-http-server.go
+++ internal/config/config-http-server.go
@@ -19,12 +19,10 @@ func GetSrvSetGlob() *ServerSettings {
 
 func NewServerSettings() *ServerSettings {
 	srvSetGlob = ServerSettings{}
-	// указываем имя флага, значение по умолчанию и описание
 	ar := flag.String("a", "localhost:8080", "address running server")
 	ab := flag.String("b", "http://localhost:8080", "base address shortener URL")
-	// делаем разбор командной строки
 	flag.Parse()
-	//проверим переменную окружения SERVER_ADDRESS
+
 	val, ok := os.LookupEnv("SERVER_ADDRESS")
 	if !ok || val == "" {
 		srvSetGlob.AdresRun = *ar
@@ -34,7 +32,6 @@ func NewServerSettings() *ServerSettings {
 		fmt.Println("AdresRun from env: ", srvSetGlob.AdresRun)
 	}
 
-	//проверим переменную окружения BASE_URL
 	val, ok = os.LookupEnv("BASE_URL")
 	if !ok || val == "" {
 		srvSetGlob.AdresBase = *ab
