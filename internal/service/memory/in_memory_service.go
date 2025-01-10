94fbf73e6155fcae9df84327177bf0d6b8c57d5c
Andrey Saperov <developerc@yandex.ru>
2025-01-10 14:03:18 +0200

    next commit15

--- internal/service/memory/in_memory_service.go
+++ internal/service/memory/in_memory_service.go
@@ -22,6 +22,7 @@ type repository interface {
 	GetLongLink(id string) (string, error)
 	HandleBatchJSON(buf bytes.Buffer) ([]byte, error)
 	AsURLExists(err error) bool
+	//GetShortURLAttr() *ShortURLAttr
 }
 
 type Service struct {
@@ -190,3 +191,7 @@ func (shu *ShortURLAttr) HandleBatchJSON(buf bytes.Buffer) ([]byte, error) {
 func (shu *ShortURLAttr) AsURLExists(err error) bool {
 	return true
 }
+
+/*func (shu *ShortURLAttr) GetShortURLAttr() *ShortURLAttr {
+	return shu
+}*/
--- internal/service/memory/reflect.go
+++ internal/service/memory/reflect.go
@@ -1,56 +1,64 @@
 package memory
 
-import (
-	"errors"
-	"log"
-	"reflect"
-)
+import "reflect"
 
 func (s *Service) GetCounter() int {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.Cntr
+	/*val := reflect.ValueOf(s.repo)
 	intCntr := val.Elem().FieldByName("Cntr").Int()
 
-	return int(intCntr)
+	return int(intCntr)*/
 }
 
 func (s *Service) IncrCounter() {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	shu.Cntr++
+	/*val := reflect.ValueOf(s.repo)
 	field := val.Elem().FieldByName("Cntr")
 	intCntr := field.Int()
 	intCntr++
 	if field.CanSet() {
 		field.SetInt(intCntr)
-	}
+	}*/
 }
 
 func (s *Service) GetAdresBase() string {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.Settings.AdresBase
+	/*val := reflect.ValueOf(s.repo)
 	settings := val.Elem().FieldByName("Settings")
 	adresBase := settings.FieldByName("AdresBase")
-	return adresBase.String()
+	return adresBase.String()*/
 }
 
 func (s *Service) GetDSN() (string, error) {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.Settings.DBStorage, nil
+	/*val := reflect.ValueOf(s.repo)
 	settings := val.Elem().FieldByName("Settings")
 	dsn := settings.FieldByName("DBStorage").String()
 	if dsn == "" {
 		return "", errors.New("get wrong DSN")
 	}
-	return dsn, nil
+	return dsn, nil*/
 }
 
 func (s *Service) GetLongURL(i int) (string, error) {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.MapURL[i], nil
+	/*val := reflect.ValueOf(s.repo)
 	mapURL := val.Elem().FieldByName("MapURL")
 	if mapURL.Kind() != reflect.Map {
 		return "", errors.New("error, this is not a Map")
 	}
-	return mapURL.MapIndex(reflect.ValueOf(i)).String(), nil
+	return mapURL.MapIndex(reflect.ValueOf(i)).String(), nil*/
 }
 
 func (s *Service) AddLongURL(i int, link string) {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	shu.MapURL[i] = link
+	/*val := reflect.ValueOf(s.repo)
 	mapURL := val.Elem().FieldByName("MapURL")
 	if mapURL.Kind() != reflect.Map {
 		log.Println("Error, this is not a Map")
@@ -60,24 +68,30 @@ func (s *Service) AddLongURL(i int, link string) {
 		log.Println("Error, can not set a Map")
 		return
 	}
-	mapURL.SetMapIndex(reflect.ValueOf(i), reflect.ValueOf(link))
+	mapURL.SetMapIndex(reflect.ValueOf(i), reflect.ValueOf(link))*/
 }
 
 func (s *Service) GetAdresRun() string {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.Settings.AdresRun
+	/*val := reflect.ValueOf(s.repo)
 	settings := val.Elem().FieldByName("Settings")
 	adresBase := settings.FieldByName("AdresRun")
-	return adresBase.String()
+	return adresBase.String()*/
 }
 
 func (s *Service) GetLogLevel() string {
-	val := reflect.ValueOf(s.repo)
+	shu := s.GetShortURLAttr()
+	return shu.Settings.LogLevel
+	/*val := reflect.ValueOf(s.repo)
 	settings := val.Elem().FieldByName("Settings")
 	adresBase := settings.FieldByName("LogLevel")
-	return adresBase.String()
+	return adresBase.String()*/
 }
 
 func (s *Service) GetShortURLAttr() *ShortURLAttr {
+	/*shu := s.GetShortURLAttr()
+	return shu*/
 	val := reflect.ValueOf(s.repo)
 	return (*ShortURLAttr)(val.UnsafePointer())
 }
