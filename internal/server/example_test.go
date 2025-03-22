51f532feb912f163918dc77da0960c76fe92d2d3
developerc <developerc@yandex.ru>
2025-03-22 12:15:24 +0300

    next commit28

---
+++ internal/server/example_test.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"fmt"
+	"net/http/httptest"
+
+	"github.com/developerc/reductorUrl/internal/service/memory"
+)
+
+func Example() {
+	svc, err := memory.NewInMemoryService()
+	if err != nil {
+		return
+	}
+	srv, err := NewServer(svc)
+	if err != nil {
+		return
+	}
+	tsrv := httptest.NewServer(srv.SetupRoutes())
+	defer tsrv.Close()
+
+	resp, _, err := svc.GetLongLink("1")
+	if resp != "http://blabla.ru" {
+		fmt.Println("test not successful!")
+	}
+}
