5df27239e2841dd92f06b91e9dec88c2420eb517
developerc <developerc@yandex.ru>
2025-03-22 08:13:20 +0300

    next commit25

--- internal/api/api.go
+++ internal/api/api.go
@@ -8,14 +8,17 @@ import (
 	"go.uber.org/zap"
 )
 
+// LongURL структура длинный URL
 type LongURL struct {
 	URL string `json:"url"`
 }
 
+// ShortURL структура короткий URL
 type ShortURL struct {
 	Result string `json:"result"`
 }
 
+// HandleAPIShorten функция демаршаллинга длинного URL
 func HandleAPIShorten(buf bytes.Buffer, logger *zap.Logger) (string, error) {
 	var longURL LongURL
 	if err := json.Unmarshal(buf.Bytes(), &longURL); err != nil {
@@ -26,6 +29,7 @@ func HandleAPIShorten(buf bytes.Buffer, logger *zap.Logger) (string, error) {
 	return longURL.URL, nil
 }
 
+// ShortToJSON функция маршаллинга короткого URL
 func ShortToJSON(strShortURL string, logger *zap.Logger) ([]byte, error) {
 	shortURL := ShortURL{Result: strShortURL}
 	jsonBytes, err := json.Marshal(shortURL)
